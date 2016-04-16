cdf0c4b26d8e94f234469a6d3fd6e476913e2dd1
James E. Marca <james@activimetrics.com>
2016-04-16 00:25:54 -0700

    committing before shutting down...

--- deploy/canonical_direction.sql
+++ deploy/canonical_direction.sql
@@ -26,5 +26,29 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION newtbmap.canonical_direction( dir varchar ) RETURNS varchar  AS
+$$
+DECLARE
+   cdirs  varchar;
+BEGIN
+   --- Convert the direction arg into a standard set we'll match
+   --- against (should really make this a separate function
+   IF ( lower(dir) IN ( 'n', 'north', 'northbound' ) ) THEN
+             cdirs = 'north';
+        ELSIF lower(dir) IN ( 's', 'south', 'southbound' ) THEN
+             cdirs = 'south';
+        ELSIF lower(dir) IN ( 'e', 'east', 'eastbound' ) THEN
+             cdirs = 'east';
+        ELSIF lower(dir) IN ( 'w', 'west', 'westbound' ) THEN
+             cdirs =  'west';
+        ELSE
+        --  RAISE NOTICE '% is an invalid direction', dir;
+        RAISE EXCEPTION '% is an invalid direction', dir;
+   END IF;
+
+   RETURN cdirs;
+END;
+$$ LANGUAGE plpgsql;
+
 
 COMMIT;
